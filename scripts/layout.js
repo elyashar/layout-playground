4649bf2a3e329bba0c0f8bbad32b7e911a0769a8
Javi Rodriguez <javier@hooray.fi>
2024-02-20 20:00:04 +0200

    fix scripts

--- scripts/layout.js
+++ scripts/layout.js
@@ -1,3 +1,4 @@
 import { addFillerText, resetFillerText } from "./filler-text";
 
 addFillerText("left-sidebar-p", "left-sidebar-add-filler-text");
+resetFillerText("left-sidebar-p", "left-sidebar-reset-filler-text");

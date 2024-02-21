fdbdb4f668b97ddaa4cde105acae919cacefa0b5
elyashar <15066886+elyashar@users.noreply.github.com>
2024-02-21 23:27:34 +0300

    add btns

--- scripts/layout.js
+++ scripts/layout.js
@@ -7,3 +7,7 @@ resetFillerText("left-sidebar-p", "left-sidebar-reset-filler-text");
 // Add event listeners to the buttons in the main content area.
 addFillerText("filler-text", "btn-add-filler-text");
 resetFillerText("filler-text", "btn-remove-filler-text");
+
+// Add event listeners to the buttons in the right sidebar.
+addFillerText("right-sidebar-p", "right-sidebar-add-filler-text");
+resetFillerText("right-sidebar-p", "right-sidebar-remove-filler-text");

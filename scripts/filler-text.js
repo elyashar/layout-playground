57d3873d5b5b0b24523ba694bfa068ac75df047e
Javi Rodriguez <javier@hooray.fi>
2024-02-21 21:37:46 +0200

    add event listeners

--- scripts/filler-text.js
+++ scripts/filler-text.js
@@ -5,25 +5,21 @@
   filler text to the paragraph.
 */
 export function addFillerText(pElemId, btnAddId) {
-  // Select the paragraph with id 'sticky-footer'.
+  // Select the paragraph with id 'pElemId'.
   let pElem = document.getElementById(pElemId);
   // console.log(pElem)
 
-  // Get the content of the paragraph.
-  const pElemInnerText = pElem.innerText;
+  const filler = `Lorem ipsum dolor sit amet consectetur adipisicing elit.
+  Fugiat dolor quam corrupti autem temporibus veniam fugit impedit facere nisi!
+  Temporibus consectetur assumenda alias aliquam ipsam ex doloremque ipsa ad 
+  saepe? Reprehenderit consequatur officiis harum amet, ratione maiores nemo, 
+  aliquid sequi doloremque eius voluptates facilis! `;
 
-  // Log the content to the console.
-  console.log(pElemInnerText);
-
-  const filler = `Lorem ipsum dolor sit amet consectetur adipisicing elit. Fugiat dolor quam corrupti autem temporibus veniam fugit impedit facere nisi! Temporibus consectetur assumenda alias aliquam ipsam ex doloremque ipsa ad saepe? Reprehenderit consequatur officiis harum amet, ratione maiores nemo, aliquid sequi doloremque eius voluptates facilis! Quam blanditiis corrupti, tempore autem voluptatem quidem, ut, totam itaque atque nesciunt nulla commodi asperiores doloribus.
-
-Lorem ipsum dolor sit amet consectetur adipisicing elit. Fugiat dolor quam corrupti autem temporibus veniam fugit impedit facere nisi! Temporibus consectetur assumenda alias aliquam ipsam ex doloremque ipsa ad saepe? Reprehenderit consequatur officiis harum amet, ratione maiores nemo, aliquid sequi doloremque eius voluptates facilis! Quam blanditiis corrupti, tempore autem voluptatem quidem, ut, totam itaque atque nesciunt nulla commodi asperiores doloribus?.`;
-
-  // Select the button
+  // Select the button  with id 'btnAddId'.
   const btn = document.getElementById(btnAddId);
 
   // Attach an event listener to the button.
-  btn.addEventListener("click", () => {
+  btn.addEventListener("click", function () {
     pElem.innerText += filler;
   });
 }
@@ -39,18 +35,11 @@ export function resetFillerText(pElemId, btnResetId) {
   let pElem = document.getElementById(pElemId);
   // console.log(pElem)
 
-  const filler = `Lorem ipsum dolor sit amet consectetur adipisicing elit. 
-  Fugiat dolor quam corrupti autem temporibus veniam fugit impedit facere nisi!
-  Temporibus consectetur assumenda alias aliquam ipsam ex doloremque ipsa ad
-  saepe? Reprehenderit consequatur officiis harum amet, ratione maiores nemo,
-  aliquid sequi doloremque eius voluptates facilis! Quam blanditiis corrupti,
-  tempore autem voluptatem quidem, ut, totam itaque atque nesciunt nulla commodi
-  asperiores doloribus. Lorem ipsum dolor sit amet consectetur adipisicing elit.`;
+  // Store a copy of the original content of the paragraph.
+  const pElemInnerText = pElem.innerText;
 
   // Select the button
   const btnRemove = document.getElementById(btnResetId);
 
-  btnRemove.addEventListener("click", () => {
-    pElem.innerText = filler;
-  });
+  btnRemove.addEventListener("click", () => pElem.innerText = pElemInnerText);
 }
--- scripts/layout.js
+++ scripts/layout.js
@@ -1,4 +1,9 @@
 import { addFillerText, resetFillerText } from "./filler-text";
 
+// Add event listeners to the buttons in the left sidebar.
 addFillerText("left-sidebar-p", "left-sidebar-add-filler-text");
 resetFillerText("left-sidebar-p", "left-sidebar-reset-filler-text");
+
+// Add event listeners to the buttons in the main content area.
+addFillerText("filler-text", "btn-add-filler-text");
+resetFillerText("filler-text", "btn-remove-filler-text");
